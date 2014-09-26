44ab77b705e6f768a5ee77a8bed744f0240d0663
wlee-nait <wlee22@studentmail.nait.ca>
2014-09-25 23:21:19 -0600

    Added Bill and Waiter class into Entities

---
+++ eRestaurantSystem/Entities/Bill.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.Entities
+{
+    public class Bill
+    {
+        [Key]
+        public int BillID { get; set; }
+        [Required(ErrorMessage = "Bill Date is required.")]
+        public DateTime BillDate { get; set; }
+        public TimeSpan OrderPlaced { get; set; }
+        [Required(ErrorMessage = "Number in party is required.")]
+        public int NumberInParty { get; set; }
+        public bool PaidStatus { get; set; }
+        [Required(ErrorMessage = "Waiter is required.")]
+        public int WaiterID { get; set; }
+        public int TableID { get; set; }
+        public int ReservationID { get; set; }
+        public bool OrderReady { get; set; }
+        [StringLength(50, ErrorMessage = "Comment has a max size of 50 characters")]
+        public string Comment { get; set; }
+
+        //navigation
+        public virtual Waiter Waiter { get; set; }
+    }
+}
---
+++ eRestaurantSystem/Entities/Waiter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.Entities
+{
+    public class Waiter
+    {
+        [Key]
+        public int WaiterID { get; set; }
+        [Required(ErrorMessage = "First Name is a required field.")]
+        [StringLength(25, ErrorMessage = "First name maximum size 25 characters")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is a required field.")]
+        [StringLength(35, ErrorMessage = "Last name maximum size 35 characters")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Phone is a required field.")]
+        [StringLength(15, ErrorMessage = "Phone maximum size 15 characters")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Address is a required field.")]
+        [StringLength(30, ErrorMessage = "Address maximum size 30 characters")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Hired Date is a required field.")]
+        public DateTime HireDate { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        //Navigation
+        public virtual ICollection<Bill> Bills
+        {
+            get;
+            set;
+        }
+    }
+}
