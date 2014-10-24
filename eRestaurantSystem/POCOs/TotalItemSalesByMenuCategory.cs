779e89483b3152bb8d345917fc28bc2ba0fa9f5b
wlee-nait <wlee22@studentmail.nait.ca>
2014-10-24 10:06:47 -0600

    Created another report

---
+++ eRestaurantSystem/POCOs/TotalItemSalesByMenuCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.POCOs
+{
+    public class TotalItemSalesByMenuCategory
+    {
+        public string CatDescription { get; set; }
+        public string ItemDescription { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
