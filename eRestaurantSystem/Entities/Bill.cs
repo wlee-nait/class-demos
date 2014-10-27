c7508a1054f96644126484ab10e71a8b2f9661ed
wlee-nait <wlee22@studentmail.nait.ca>
2014-10-27 16:18:17 -0600

    Added Clockpicker source
    Added Clockpicker and standalone css and clockpicker jscript to project

--- eRestaurantSystem/Entities/Bill.cs
+++ eRestaurantSystem/Entities/Bill.cs
@@ -14,13 +14,16 @@ namespace eRestaurantSystem.Entities
         [Required(ErrorMessage = "Bill Date is required.")]
         public DateTime BillDate { get; set; }
         public TimeSpan OrderPlaced { get; set; }
+        public TimeSpan OrderReady { get; set; }
+        public TimeSpan OrderServed { get; set; }
+        public TimeSpan OrderPaid { get; set; }
         [Required(ErrorMessage = "Number in party is required.")]
         public int NumberInParty { get; set; }
         public bool PaidStatus { get; set; }
         [Required(ErrorMessage = "Waiter is required.")]
         public int WaiterID { get; set; }
-        public int TableID { get; set; }
-        public int ReservationID { get; set; }
+        public int? TableID { get; set; }
+        public int? ReservationID { get; set; }
         public bool OrderReady { get; set; }
         [StringLength(50, ErrorMessage = "Comment has a max size of 50 characters")]
         public string Comment { get; set; }
